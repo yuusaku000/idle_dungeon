4a566f9b8f8bfeeb8f442e7e72957c5c33985ebe
yuusaku000 <takakura.yusaku.25@shizuoka.ac.jp>
2026-08-24 19:43:56 +0900

    feat: ホーム画面の文言を変更

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('test'),
+            const Text('hehehe,kaeteyattaze'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
